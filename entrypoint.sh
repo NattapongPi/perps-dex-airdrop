b41ab3f20fc5bf4295e8d8952f0f15f7d83066dc
NattapongPi <nattapong.srk@gmail.com>
2026-04-19 03:43:14 +0700

    fix: export container env vars to /etc/environment so cron jobs inherit them

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Dump container env vars so cron jobs can see them (cron runs in a clean shell).
+printenv | grep -v "^_=" >> /etc/environment
 # Start cron daemon in background, then run health server in foreground.
 cron
 exec python3 -m src.health_server
